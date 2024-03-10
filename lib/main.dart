5350834f15e54db3c96c379ab7fbd42a1901a959
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-10 22:23:14 +0800

    Added background

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,43 @@ class LoginePage extends StatefulWidget {
 class _LoginePageState extends State<LoginePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 400,
+            child: Stack(
+              children: [
+                Positioned(
+                  height: 400,
+                  top: -40,
+                  width: width,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/background.png"),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  height: 400,
+                  width: width + 20,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/background-2.png"),
+                          fit: BoxFit.fill),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
