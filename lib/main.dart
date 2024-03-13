e434aacf9fcdb09530f054db0d4af0ccbc4d198a
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-13 20:12:06 +0800

    Implemented Login Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -20,11 +22,13 @@ class _LoginePageState extends State<LoginePage> {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 400,
             child: Stack(
               children: [
+                //Purple background
                 Positioned(
                   height: 400,
                   top: -40,
@@ -38,6 +42,8 @@ class _LoginePageState extends State<LoginePage> {
                     ),
                   ),
                 ),
+
+                //Purple flower background
                 Positioned(
                   height: 400,
                   width: width + 20,
@@ -46,6 +52,109 @@ class _LoginePageState extends State<LoginePage> {
                       image: DecorationImage(
                           image: AssetImage("assets/images/background-2.png"),
                           fit: BoxFit.fill),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          SizedBox(height: 30),
+
+          //Login Text
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Color.fromRGBO(49, 39, 79, 1),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+
+                SizedBox(height: 40),
+
+                //
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(196, 135, 198, .4),
+                          blurRadius: 20,
+                          offset: Offset(0, 10))
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                          color: Colors.grey.shade300,
+                        ))),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Username",
+                              hintStyle: TextStyle(color: Colors.grey)),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                //Forgot Password Text
+                Center(
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(color: Color.fromRGBO(196, 135, 198, 1)),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                //Login Button
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.symmetric(horizontal: 50),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color.fromRGBO(49, 39, 79, 1),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                //Create Account
+                Center(
+                  child: Text(
+                    "Create Account",
+                    style: TextStyle(
+                      color: Color.fromRGBO(49, 39, 79, .6),
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 )
